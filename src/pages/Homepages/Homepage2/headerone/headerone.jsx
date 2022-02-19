5d9e4d469e5046e6be20aeab83e3da3ae3b9cfc3
Priyansh Maurya <priyansh.maurya@gmail.com>
2022-02-19 18:43:22 +0530

    Navbar 1

--- src/pages/Homepages/Homepage2/headerone/headerone.jsx
+++ src/pages/Homepages/Homepage2/headerone/headerone.jsx
@@ -2,46 +2,81 @@ import React from "react";
 import "./style.scss";
 import { Link } from "gatsby";
 import logo from "../../../../images/thenerdseries.png";
+import WeatherIcon from "../../../../images/icons/weather";
+import FacebookIcon from "../../../../images/icons/facebook";
+import TwitterIcon from "../../../../images/icons/twitter";
+import YoutubeIcon from "../../../../images/icons/youtube";
+import SearchIcon from "../../../../images/icons/search";
 
-const Header = ({ category }) => {
+const Header1 = ({ category }) => {
   return (
-    <header className="header">
+    <header>
       <div className="container is-max-widescreen">
-        <nav className="navbar" role="navigation" aria-label="main navigation">
+        {/* <nav className="navbar" role="navigation" aria-label="main navigation">
           <div className="navbar-brand">
             <a className="navbar-item" href="https://bulma.io">
               <img src={logo} alt="logo" className="nav-logo" />
             </a>
-            <a
-              role="button"
-              className="navbar-burger"
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="navbarBasicExample"
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a>
+          </div>
+        </nav> */}
+
+        <nav className="top-nav is-flex is-justify-content-space-between">
+          <ul className="top-nav-ul">
+            <li>Advertise</li>
+            <li>About</li>
+            <li>Events</li>
+            <li>Write for us</li>
+          </ul>
+          <span> Sign in/Join </span>
+        </nav>
+
+        <div className="mid-nav is-flex">
+          <div className="mid-left-nav">
+            <WeatherIcon />
+            <b>3.8 </b>
+            <sup>C</sup>
+            <span> Munich </span>
           </div>
 
-          <div className="navbar-menu">
-            <div className="navbar-end">
-              <Link to={`/`} className="navbar-item">
-                Home
-              </Link>
-              {category?.map((categoryData) => {
-                return (
-                  <Link to={`/${categoryData.slug}`} className="navbar-item">
-                    {categoryData?.name}
-                  </Link>
-                );
-              })}
-            </div>
+          <div className="mid-mid-nav has-text-centered">
+            <p className="header-heading has-text-centered is-uppercase"> The Daily Bugle</p>
+            <p className="header-date has-text-centered"> Saturday, February 19, 2022</p>
           </div>
+
+          <div className="mid-right-nav">
+            <ul>
+              <li>
+                <FacebookIcon />
+              </li>
+              <li>
+                <TwitterIcon />
+              </li>
+              <li>
+                <YoutubeIcon />
+              </li>
+              <li>
+                <SearchIcon />
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <nav className="bottom-nav">
+        <hr />
+          <ul className="bottom-nav-ul is-flex is-flex-wrap-wrap is-justify-content-space-around has-text-weight-bold is-uppercase">
+            <li>Home</li>
+            <li>News</li>
+            <li>Magazine</li>
+            <li>Sports</li>
+            <li>Style</li>
+            <li>Arts</li>
+            <li>Culture</li>
+            <li>Politics</li>
+          </ul>
+        <hr />
         </nav>
       </div>
     </header>
   );
 };
-export default Header;
+export default Header1;
--- src/pages/Homepages/Homepage2/index.js
+++ src/pages/Homepages/Homepage2/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../../pages/mystyles.scss";
-import Header from "./headerone/headerone";
+import Header1 from "./headerone/headerone";
 import Sec1 from "./sec1/sec1";
 import Sec2 from "./sec2/sec2";
 import Sec3 from "./sec3/sec3";
@@ -10,7 +10,7 @@ import Sec5 from "./sec5/sec5";
 const Homepage2 = () => {
   return (
     <div className="layout">
-      <Header />
+      <Header1 />
       <Sec1 />
       <Sec2 />
       <Sec3 />
